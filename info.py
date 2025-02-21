304bda819fe0fac794bd03540837cd9001971fb9
skbots001 <skbots001@gmail.com>
2025-02-21 17:15:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+rh4V2xYC-1hjND
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002340172655').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FlixgenieBot:FlixgenieBot@flixgeniebot.qdvjz.mongodb.net/?retryWrites=true&w=majority&appName=FlixgenieBot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "FlixgenieBot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
