48a303741b4d3c7b95e0a76715aefcbb10e113fd
skbots001 <skbots001@gmail.com>
2025-02-21 17:16:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7885265371:AAEwb0eP4WkP5JJkomWYZnMnMNnEqeP
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6602755984').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/skbots0") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002459597070'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+rh4V2xYC-1hjNDI1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002340172655').split()]
